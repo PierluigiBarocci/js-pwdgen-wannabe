66fe194fc85df4b8bfb1e3b98a7b685076f146b5
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-13 15:44:34 +0100

    Si presenta interessante, ma bisogna stilarlo un po'

--- main.js
+++ main.js
@@ -9,16 +9,17 @@ var ageString = age.toString();
 // occhio: parte a contare da 0, come in una gara.
 var res1 = ageString.charAt(2);
 var res2 = ageString.charAt(3);
-var togheter = name + surname + colour + res1 + res2;
-
-// prove in console
-console.log(name);
-console.log(surname);
-console.log(colour);
-console.log(togheter);
-console.log(ageString);
-console.log(res1);
-console.log(res2);
+var resName1 = name.charAt(0);
+var resName2 = name.charAt(1);
+var resName3 = name.charAt(2);
+var resSurname1 = surname.charAt(0);
+var resSurname2 = surname.charAt(1);
+var resSurname3 = surname.charAt(2);
+var resColour1 = colour.charAt(0);
+var resColour2 = colour.charAt(1);
+var togheter = name + surname + colour;
+var togheterShort = resName1 + resName2 +resName3 + resSurname1 + resSurname2 + resSurname3 + resColour1 + resColour2 + res1 + res2;
 
 // comando per inserire la password in html
 document.getElementById('text').innerHTML = togheter;
+document.getElementById('textShort').innerHTML = togheterShort;
